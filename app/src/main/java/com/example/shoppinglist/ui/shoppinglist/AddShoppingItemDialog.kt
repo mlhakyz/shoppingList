2d339a3837d8faaf99d841e772b08631e1fc3ef4
Melih Akyuz <melihakyuz@Melihs-MacBook-Pro.local>
2023-11-23 17:44:14 +0300

    add items with a custom dialog

---
+++ app/src/main/java/com/example/shoppinglist/ui/shoppinglist/AddShoppingItemDialog.kt
@@ -0,0 +1,40 @@
+package com.example.shoppinglist.ui.shoppinglist
+
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDialog
+import com.example.shoppinglist.Data.db.entities.ShoppingItem
+import com.example.shoppinglist.R
+import kotlinx.android.synthetic.main.dialog_add_shopping_item.etAmount
+import kotlinx.android.synthetic.main.dialog_add_shopping_item.etName
+import kotlinx.android.synthetic.main.dialog_add_shopping_item.tvAdd
+import kotlinx.android.synthetic.main.dialog_add_shopping_item.tvCancel
+
+class AddShoppingItemDialog(context: Context, var addDialogListener: AddDialogListener) : AppCompatDialog(context) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_add_shopping_item)
+
+
+         tvAdd.setOnClickListener {
+            val name = etName.text.toString()
+            val amount = etAmount.text.toString()
+
+            if(name.isEmpty() || amount.isEmpty()){
+                Toast.makeText(context,"Please enter all the information",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val item = ShoppingItem(name,amount.toInt())
+            addDialogListener.onAddButtonClicked(item)
+            dismiss()
+        }
+        tvCancel.setOnClickListener {
+            cancel()
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -2,10 +2,16 @@ package com.example.shoppinglist.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shoppinglist.Data.db.ShoppingDatabese
+import com.example.shoppinglist.Data.db.entities.ShoppingItem
 import com.example.shoppinglist.Data.repositories.ShoppingRepository
 import com.example.shoppinglist.R
+import com.example.shoppinglist.other.ShoppingItemAdapter
+import kotlinx.android.synthetic.main.activity_shopping.fab
+import kotlinx.android.synthetic.main.activity_shopping.rvShoppingItems
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +23,26 @@ class ShoppingActivity : AppCompatActivity() {
         val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this,factory).get(ShoppingViewModel::class.java)
+
+        val adapter = ShoppingItemAdapter(listOf(),viewModel)
+
+        rvShoppingItems.layoutManager = LinearLayoutManager(this)
+        rvShoppingItems.adapter = adapter
+
+        viewModel.getAllShoppingItems().observe(this, Observer {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        })
+
+        fab.setOnClickListener {
+            AddShoppingItemDialog(this,
+                object : AddDialogListener{
+                    override fun onAddButtonClicked(item: ShoppingItem) {
+                       viewModel.upsert(item)
+                    }
+
+                }).show()
+        }
+
     }
 }
