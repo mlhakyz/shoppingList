e89e44531413fd1a6a5dd511a8a6c9a6aded5354
Melih Akyuz <melihakyuz@Melihs-MacBook-Pro.local>
2023-11-23 11:25:09 +0300

    shopping Item, DAO and SQLITE Database

--- app/src/main/java/com/example/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingActivity.kt
@@ -1,11 +1,11 @@
-package com.example.shoppinglist 
+package com.example.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
     }
 }
---
+++ app/src/main/java/com/example/shoppinglist/ShoppingDatabese.kt
@@ -0,0 +1,29 @@
+package com.example.shoppinglist
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ShoppingItem::class],
+    version = 1
+)
+abstract class ShoppingDatabese: RoomDatabase() {
+
+    abstract fun getShoppingDao(): ShoppingDao
+
+    companion object{
+        @Volatile
+        private var instance: ShoppingDatabese? = null
+        private val Lock = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(Lock){
+            instance ?: createDatabase(context).also { instance = it }
+        }
+        private fun createDatabase(context:Context) =
+            Room.databaseBuilder(context.applicationContext,
+                ShoppingDatabese::class.java,"ShoppingDB.db").build()
+    }
+
+}
